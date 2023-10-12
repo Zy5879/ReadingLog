11877b9b3a7dfaee393afe9563b4dd8b446a9ddb
Zy5879 <ZaireM21@gmail.com>
2023-10-12 14:23:48 -0400

    created my some of my library views

--- Library/View/LibraryRowView.swift
+++ Library/View/LibraryRowView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct LibraryRowView: View {
+    let books: Array<String> = ["Harry Potter", "Percy Jackson"]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(books, id: \.self) { book in
+                Text(book)
+            }
+        }
     }
 }
 
---
+++ Library/View/LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  Library
+//
+//  Created by Zaire McAllister on 10/12/23.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text("Main Library")
+    }
+}
+
+#Preview {
+    LibraryView()
+}
