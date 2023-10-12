b2f9411e50e2e8617ec9bfbaad342a5ad1d271e2
Zy5879 <ZaireM21@gmail.com>
2023-10-12 16:11:00 -0400

    Fixed UI of Library Add Book View

--- Library/View/LibraryAddBook.swift
+++ Library/View/LibraryAddBook.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct LibraryAddBook: View {
+    @State var title: String = ""
+    @State var author: String = ""
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                TextField("Title of Book", text: $title)
+                    .frame(height: 55)
+                    .padding(.horizontal)
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+                
+                TextField("Name of Author", text: $author)
+                    .frame(height: 55)
+                    .padding(.horizontal)
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+                    Button(action: {
+                    
+                }, label: {
+                    Text("Save Book")
+                })
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.accentColor)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+                    .padding(20)
+            }
+            .padding(20)
+        }
+        .navigationTitle("Add Book 📖")
     }
 }
 
 #Preview {
-    LibraryAddBook()
+    NavigationStack {
+        LibraryAddBook()
+    }
 }
--- Library/View/LibraryRowView.swift
+++ Library/View/LibraryRowView.swift
@@ -17,7 +17,6 @@ struct LibraryRowView: View {
                 Text(book)
             }
             Text("Author: Rick Riordan")
-            Text("Current Page Number: 90")
         }
     }
 }
