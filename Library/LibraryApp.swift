f641e794ec37da62781bb3dacdb0bbadbd15d2d4
Zy5879 <ZaireM21@gmail.com>
2023-10-12 14:06:59 -0400

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Zaire McAllister on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LibraryRowView()
+        }
+    }
+}
